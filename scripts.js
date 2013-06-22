0002fc054b923008f9d9e73d22cfcba7a427f2c9
Dverbrugge <don@mvdesigngroupinc.com>
2013-06-22 14:20:39 -0700

    new code added
    changed code to reflect proper syntax

--- scripts.js
+++ scripts.js
@@ -1,29 +1,17 @@
-$(document).ready(function($) {
 
+$(document).ready(function(){
+  $("form").submit(function(event){
+	event.preventDefault();
 
-	$("form").submit(function)(event){
-		// alert("form submitted");
-		event.preventDefault();
-		var Noun =$ ("#Mary had a little lamb").val();
-		var Verb =$ ("#whose fleece was ").val();
-		var phone number =$ ("#phone number").val();
-		var email address =$ ("#email address").val();
-		var arrival date  =$ ("#arrival date").val();
-		var length of stay  =$ ("#length of stay").val();
-
-		$("#Noun val").text("Mary: "+ little lamb );
-		$("#last name val").text("Last name: "+ last name);
-		$("#phone number val").text("Phone number: "+ phone number );
-		$("#email address val").text("Email address: "+ email address );
-		$("#arrival date val").text("Arrival date: "+ arrival date );
-		$("#length of stay val").text("Length of stay: "+ length of stay );
-		
-
-
-		$(this).hide();
-		
-	};
+  	var adverb = $("#adv").val()
+	var adjective = $("#adj").val()
+	var noun = $("#noun").val()
+	var verb = $("#verb").val()
+	var pronoun = $("pronoun").val()
+	
+	$("form").hide()
+	$("h1").text("How to find Mary's lamb");
+	$("#story").text("Mary had a little lamb "+pronoun+". whose fleece was white as snow "+adverb+". and every where Mary went  "+adjective+". it was sure to go "+verb+". it followed Mary ");
+    
+  });
 });
-
-
-j
